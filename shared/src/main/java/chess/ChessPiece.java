eac4220fcf88762043bee7c24daa54d136c70647
ceberle00 <112594166+ceberle00@users.noreply.github.com>
2024-05-07 15:48:52 -0600

    rook&Queen

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -730,7 +730,298 @@ public class ChessPiece {
                 }
             } 
         }
-            
+        else if (this.pieceType == PieceType.QUEEN) {
+            //this'll require for loop :(
+            //all diagnol ones or whatevs
+            for (int i = column+1, j=rows+1; i < 9 && j < 9; i++, j++) {
+                ChessPosition pos = new ChessPosition(j, i);
+                //if there isn't a 
+                if (board.getPiece(pos) == null)
+                {
+                    //create new move
+                    ChessMove move = new ChessMove(myPosition, pos, null); //maybe switch piecetype to null?
+                    moves.add(move);
+                }
+                else {
+                    ChessPiece pieceBlocked = board.getPiece(pos);
+                    ChessPiece currPiece = board.getPiece(myPosition);
+                    if (currPiece.getTeamColor() == pieceBlocked.getTeamColor()) {
+                        break;
+                    }
+                    else {
+                        ChessMove move = new ChessMove(myPosition, pos, null); //checking this
+                        moves.add(move);
+                        break;
+                    }
+                    //add the move, then break?
+                    //for now not adding move
+                }
+            } 
+            for (int i = column+1, j = rows-1; i<9 && j>0; i++,j-- ) 
+            {
+                ChessPosition pos = new ChessPosition(j, i);
+                //if there isn't a 
+                if (board.getPiece(pos) == null)
+                {
+                    //create new move
+                    ChessMove move = new ChessMove(myPosition, pos, null); //maybe switch piecetype to null?
+                    moves.add(move);
+                }
+                else {
+                    ChessPiece pieceBlocked = board.getPiece(pos);
+                    ChessPiece currPiece = board.getPiece(myPosition);
+                    if (currPiece.getTeamColor() == pieceBlocked.getTeamColor()) {
+                        break;
+                    }
+                    else {
+                        ChessMove move = new ChessMove(myPosition, pos, null); //checking this
+                        moves.add(move);
+                        break;
+                    }
+                }
+            }
+            for (int i = column-1, j = rows+1; i>0 && j<9;i--,j++ ) 
+            {
+                ChessPosition pos = new ChessPosition(j, i);
+                //if there isn't a 
+                if (board.getPiece(pos) == null)
+                {
+                    //create new move
+                    ChessMove move = new ChessMove(myPosition, pos, null); //maybe switch piecetype to null?
+                    moves.add(move);
+                }
+                else {
+                    ChessPiece pieceBlocked = board.getPiece(pos);
+                    ChessPiece currPiece = board.getPiece(myPosition);
+                    if (currPiece.getTeamColor() == pieceBlocked.getTeamColor()) {
+                        break;
+                    }
+                    else {
+                        ChessMove move = new ChessMove(myPosition, pos, null); //checking this
+                        moves.add(move);
+                        break;
+                    }
+                }
+            }
+            for (int i = column-1, j = rows-1; i>0 && j>0; i--,j-- ) 
+            {
+                ChessPosition pos = new ChessPosition(j, i);
+                //if there isn't a 
+                if (board.getPiece(pos) == null)
+                {
+                    //create new move
+                    ChessMove move = new ChessMove(myPosition, pos, null); //maybe switch piecetype to null?
+                    moves.add(move);
+                }
+                else {
+                    ChessPiece pieceBlocked = board.getPiece(pos);
+                    ChessPiece currPiece = board.getPiece(myPosition);
+                    if (currPiece.getTeamColor() == pieceBlocked.getTeamColor()) {
+                        break;
+                    }
+                    else {
+                        ChessMove move = new ChessMove(myPosition, pos, null); //checking this
+                        moves.add(move);
+                        break;
+                    }
+                }
+            }
+            for (int i = column+1; i < 9; i++) {
+                ChessPosition pos = new ChessPosition(rows, i);
+                //if there isn't a 
+                if (board.getPiece(pos) == null)
+                {
+                    //create new move
+                    ChessMove move = new ChessMove(myPosition, pos, null); //maybe switch piecetype to null?
+                    moves.add(move);
+                }
+                else {
+                    ChessPiece pieceBlocked = board.getPiece(pos);
+                    ChessPiece currPiece = board.getPiece(myPosition);
+                    if (currPiece.getTeamColor() == pieceBlocked.getTeamColor()) {
+                        break;
+                    }
+                    else {
+                        ChessMove move = new ChessMove(myPosition, pos, null); //checking this
+                        moves.add(move);
+                        break;
+                    }
+                    //add the move, then break?
+                    //for now not adding move
+                }
+            } 
+            for (int i = column-1; i > 0; i--) {
+                ChessPosition pos = new ChessPosition(rows, i);
+                //if there isn't a 
+                if (board.getPiece(pos) == null)
+                {
+                    //create new move
+                    ChessMove move = new ChessMove(myPosition, pos, null); //maybe switch piecetype to null?
+                    moves.add(move);
+                }
+                else {
+                    ChessPiece pieceBlocked = board.getPiece(pos);
+                    ChessPiece currPiece = board.getPiece(myPosition);
+                    if (currPiece.getTeamColor() == pieceBlocked.getTeamColor()) {
+                        break;
+                    }
+                    else {
+                        ChessMove move = new ChessMove(myPosition, pos, null); //checking this
+                        moves.add(move);
+                        break;
+                    }
+                    //add the move, then break?
+                    //for now not adding move
+                }
+            } 
+            for (int i = rows+1; i < 9; i++) {
+                ChessPosition pos = new ChessPosition(i, column);
+                //if there isn't a 
+                if (board.getPiece(pos) == null)
+                {
+                    //create new move
+                    ChessMove move = new ChessMove(myPosition, pos, null); //maybe switch piecetype to null?
+                    moves.add(move);
+                }
+                else {
+                    ChessPiece pieceBlocked = board.getPiece(pos);
+                    ChessPiece currPiece = board.getPiece(myPosition);
+                    if (currPiece.getTeamColor() == pieceBlocked.getTeamColor()) {
+                        break;
+                    }
+                    else {
+                        ChessMove move = new ChessMove(myPosition, pos, null); //checking this
+                        moves.add(move);
+                        break;
+                    }
+                    //add the move, then break?
+                    //for now not adding move
+                }
+            } 
+            for (int i = rows-1; i > 0; i--) {
+                ChessPosition pos = new ChessPosition(i, column);
+                //if there isn't a 
+                if (board.getPiece(pos) == null)
+                {
+                    //create new move
+                    ChessMove move = new ChessMove(myPosition, pos, null); //maybe switch piecetype to null?
+                    moves.add(move);
+                }
+                else {
+                    ChessPiece pieceBlocked = board.getPiece(pos);
+                    ChessPiece currPiece = board.getPiece(myPosition);
+                    if (currPiece.getTeamColor() == pieceBlocked.getTeamColor()) {
+                        break;
+                    }
+                    else {
+                        ChessMove move = new ChessMove(myPosition, pos, null); //checking this
+                        moves.add(move);
+                        break;
+                    }
+                    //add the move, then break?
+                    //for now not adding move
+                }
+            } 
+        }
+        else if (this.pieceType == PieceType.ROOK) 
+        {
+            for (int i = column+1; i < 9; i++) {
+                ChessPosition pos = new ChessPosition(rows, i);
+                //if there isn't a 
+                if (board.getPiece(pos) == null)
+                {
+                    //create new move
+                    ChessMove move = new ChessMove(myPosition, pos, null); //maybe switch piecetype to null?
+                    moves.add(move);
+                }
+                else {
+                    ChessPiece pieceBlocked = board.getPiece(pos);
+                    ChessPiece currPiece = board.getPiece(myPosition);
+                    if (currPiece.getTeamColor() == pieceBlocked.getTeamColor()) {
+                        break;
+                    }
+                    else {
+                        ChessMove move = new ChessMove(myPosition, pos, null); //checking this
+                        moves.add(move);
+                        break;
+                    }
+                    //add the move, then break?
+                    //for now not adding move
+                }
+            } 
+            for (int i = column-1; i > 0; i--) {
+                ChessPosition pos = new ChessPosition(rows, i);
+                //if there isn't a 
+                if (board.getPiece(pos) == null)
+                {
+                    //create new move
+                    ChessMove move = new ChessMove(myPosition, pos, null); //maybe switch piecetype to null?
+                    moves.add(move);
+                }
+                else {
+                    ChessPiece pieceBlocked = board.getPiece(pos);
+                    ChessPiece currPiece = board.getPiece(myPosition);
+                    if (currPiece.getTeamColor() == pieceBlocked.getTeamColor()) {
+                        break;
+                    }
+                    else {
+                        ChessMove move = new ChessMove(myPosition, pos, null); //checking this
+                        moves.add(move);
+                        break;
+                    }
+                    //add the move, then break?
+                    //for now not adding move
+                }
+            } 
+            for (int i = rows+1; i < 9; i++) {
+                ChessPosition pos = new ChessPosition(i, column);
+                //if there isn't a 
+                if (board.getPiece(pos) == null)
+                {
+                    //create new move
+                    ChessMove move = new ChessMove(myPosition, pos, null); //maybe switch piecetype to null?
+                    moves.add(move);
+                }
+                else {
+                    ChessPiece pieceBlocked = board.getPiece(pos);
+                    ChessPiece currPiece = board.getPiece(myPosition);
+                    if (currPiece.getTeamColor() == pieceBlocked.getTeamColor()) {
+                        break;
+                    }
+                    else {
+                        ChessMove move = new ChessMove(myPosition, pos, null); //checking this
+                        moves.add(move);
+                        break;
+                    }
+                    //add the move, then break?
+                    //for now not adding move
+                }
+            } 
+            for (int i = rows-1; i > 0; i--) {
+                ChessPosition pos = new ChessPosition(i, column);
+                //if there isn't a 
+                if (board.getPiece(pos) == null)
+                {
+                    //create new move
+                    ChessMove move = new ChessMove(myPosition, pos, null); //maybe switch piecetype to null?
+                    moves.add(move);
+                }
+                else {
+                    ChessPiece pieceBlocked = board.getPiece(pos);
+                    ChessPiece currPiece = board.getPiece(myPosition);
+                    if (currPiece.getTeamColor() == pieceBlocked.getTeamColor()) {
+                        break;
+                    }
+                    else {
+                        ChessMove move = new ChessMove(myPosition, pos, null); //checking this
+                        moves.add(move);
+                        break;
+                    }
+                    //add the move, then break?
+                    //for now not adding move
+                }
+            }
+        }
         return moves;
     }
 }
